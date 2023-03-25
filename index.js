73907f792dedf244bcf6fa820a9832d2c8b5e1fb
Henry-Tourraine <henry.tourraine@outlook.fr>
2023-03-25 23:00:03 +0100

    sixth

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let http = require('http');
 let crypto = require('crypto');
 let  { exec } = require('child_process');
 const express = require('express');
-const {createUserFiles} = reuqire("./docs.js");
+const {createUserFiles} = require("./docs.js");
 const app = express();
 // you explicitly create the http server
 const server = require('http').createServer(app);
