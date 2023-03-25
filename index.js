6bc4bfe16deddf01c608720ea5b1bdfb9c40f842
Henry-Tourraine <henry.tourraine@outlook.fr>
2023-03-25 22:02:34 +0100

    fourth commit

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post("/", (req, res) => {
         console.log("allowed")
         const { exec } = require('child_process');
 
-        exec('cd ../setup.sh', (error, stdout, stderr) => {
+        exec('cd ../launch.sh', (error, stdout, stderr) => {
         if (error) {
             console.error(`error: ${error.message}`);
             return;
